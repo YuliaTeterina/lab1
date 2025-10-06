e4b30d4fa8a5d5308cfb7b8929f5fb436f45df21
Julia <um46970@gmail.com>
2025-10-06 17:58:59 +0500

    Google Java Style

--- src/Main.java
+++ src/Main.java
@@ -47,7 +47,7 @@ public class Main {
                     int b = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.islnRange(a,b,num));
+                    System.out.println(solution.isInRange(a, b, num));
                     break;
                 case 5:
                     System.out.println("Введите первое число: ");
@@ -60,161 +60,170 @@ public class Main {
                     int n3 = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.isEqual(n1,n2,n3));
+                    System.out.println(solution.isEqual(n1, n2, n3));
                     break;
                 case 6:
                     System.out.println("Введите число: ");
-                    int abs_a = scanner.nextInt();
+                    int absA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.abs(abs_a));
+                    System.out.println(solution.abs(absA));
+                    break;
                 case 7:
                     System.out.println("Введите число: ");
-                    int is35_a = scanner.nextInt();
+                    int is35A = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.is35(is35_a));
+                    System.out.println(solution.is35(is35A));
+                    break;
                 case 8:
                     System.out.println("Введите первое число: ");
-                    int max3_1 = scanner.nextInt();
+                    int max31 = scanner.nextInt();
 
                     System.out.println("Введите второе число: ");
-                    int max3_2 = scanner.nextInt();
+                    int max32 = scanner.nextInt();
 
                     System.out.println("Введите третье число: ");
-                    int max3_3 = scanner.nextInt();
+                    int max33 = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.max3(max3_1, max3_2, max3_3));
+                    System.out.println(solution.max3(max31, max32, max33));
+                    break;
                 case 9:
                     System.out.println("Введите первое число: ");
-                    int sum2_1 = scanner.nextInt();
+                    int sum21 = scanner.nextInt();
 
                     System.out.println("Введите второе число: ");
-                    int sum2_2 = scanner.nextInt();
+                    int sum22 = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.sum2(sum2_1, sum2_2));
+                    System.out.println(solution.sum2(sum21, sum22));
+                    break;
                 case 10:
                     System.out.println("Введите число: ");
-                    int day_a = scanner.nextInt();
+                    int dayA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.day(day_a));
+                    System.out.println(solution.day(dayA));
+                    break;
                 case 11:
                     System.out.println("Введите число: ");
-                    int listNums_a = scanner.nextInt();
+                    int listNumsA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.listNums(listNums_a));
+                    System.out.println(solution.listNums(listNumsA));
+                    break;
                 case 12:
                     System.out.println("Введите число: ");
-                    int chet_a = scanner.nextInt();
+                    int chetA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.chet(chet_a));
+                    System.out.println(solution.chet(chetA));
+                    break;
                 case 13:
                     System.out.println("Введите число: ");
-                    int numLen_a = scanner.nextInt();
+                    int numLenA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.numLen(numLen_a));
+                    System.out.println(solution.numLen(numLenA));
+                    break;
                 case 14:
                     System.out.println("Введите число: ");
-                    int square_a = scanner.nextInt();
+                    int squareA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    solution.square(square_a);
+                    solution.square(squareA);
+                    break;
                 case 15:
                     System.out.println("Введите число: ");
-                    int triangle_a = scanner.nextInt();
+                    int triangleA = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    solution.rightTriangle(triangle_a);
+                    solution.rightTriangle(triangleA);
+                    break;
                 case 16:
                     System.out.println("Введите длину массива: ");
-                    int arr_len = scanner.nextInt();
-                    int[] arr = new int[arr_len];
+                    int arrLen = scanner.nextInt();
+                    int[] arr = new int[arrLen];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_len; i++){
+                    for (int i = 0; i < arrLen; i++) {
                         arr[i] = scanner.nextInt();
                     }
 
                     System.out.println("Введите искомое число: ");
-                    int arr_find = scanner.nextInt();
+                    int arrFind = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(solution.findFirst(arr, arr_find));
+                    System.out.println(solution.findFirst(arr, arrFind));
+                    break;
                 case 17:
                     System.out.println("Введите длину массива: ");
-                    int arr_len2 = scanner.nextInt();
-                    int[] arr2 = new int[arr_len2];
+                    int arrLen2 = scanner.nextInt();
+                    int[] arr2 = new int[arrLen2];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_len2; i++){
+                    for (int i = 0; i < arrLen2; i++) {
                         arr2[i] = scanner.nextInt();
                     }
 
                     System.out.print("Результат: ");
                     System.out.println(solution.maxAbs(arr2));
+                    break;
                 case 18:
                     System.out.println("Введите длину 1-го массива: ");
-                    int arr_first_len = scanner.nextInt();
-                    int[] arr_first = new int[arr_first_len];
+                    int arrFirstLen = scanner.nextInt();
+                    int[] arrFirst = new int[arrFirstLen];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_first_len; i++){
-                        arr_first[i] = scanner.nextInt();
+                    for (int i = 0; i < arrFirstLen; i++) {
+                        arrFirst[i] = scanner.nextInt();
                     }
 
                     System.out.println("Введите длину 2-го массива: ");
-                    int arr_second_len = scanner.nextInt();
-                    int[] arr_second = new int[arr_second_len];
+                    int arrSecondLen = scanner.nextInt();
+                    int[] arrSecond = new int[arrSecondLen];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_second_len; i++){
-                        arr_second[i] = scanner.nextInt();
+                    for (int i = 0; i < arrSecondLen; i++) {
+                        arrSecond[i] = scanner.nextInt();
                     }
 
                     System.out.println("Введите позицию вставки: ");
                     int pos = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    /*int[] arr_res = solution.add(arr_first, arr_second, pos);
-                    for(int i=0; i < arr_first_len + arr_second_len; i++){
-                        System.out.print(arr_res[i]);
-                        System.out.print(" ");
-                    }
-                    System.out.println();*/
-                    System.out.println(Arrays.toString(solution.add(arr_first, arr_second, pos)));
+                    System.out.println(Arrays.toString(solution.add(arrFirst, arrSecond, pos)));
+                    break;
                 case 19:
                     System.out.println("Введите длину массива: ");
-                    int arr_reverse_len = scanner.nextInt();
-                    int[] arr_reverse = new int[arr_reverse_len];
+                    int arrReverseLen = scanner.nextInt();
+                    int[] arrReverse = new int[arrReverseLen];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_reverse_len; i++){
-                        arr_reverse[i] = scanner.nextInt();
+                    for (int i = 0; i < arrReverseLen; i++) {
+                        arrReverse[i] = scanner.nextInt();
                     }
 
                     System.out.print("Результат: ");
-                    System.out.println(Arrays.toString(solution.reverseBack(arr_reverse)));
+                    System.out.println(Arrays.toString(solution.reverseBack(arrReverse)));
+                    break;
                 case 20:
                     System.out.println("Введите длину массива: ");
-                    int arr_findAll_len = scanner.nextInt();
-                    int[] arr_findAll = new int[arr_findAll_len];
+                    int arrFindAllLen = scanner.nextInt();
+                    int[] arrFindAll = new int[arrFindAllLen];
 
                     System.out.println("Вводите числа по одному: ");
-                    for (int i = 0; i < arr_findAll_len; i++){
-                        arr_findAll[i] = scanner.nextInt();
+                    for (int i = 0; i < arrFindAllLen; i++) {
+                        arrFindAll[i] = scanner.nextInt();
                     }
 
                     System.out.println("Введите искомое число: ");
-                    int arr_findAll_find = scanner.nextInt();
+                    int arrFindAllFind = scanner.nextInt();
 
                     System.out.print("Результат: ");
-                    System.out.println(Arrays.toString(solution.findAll(arr_findAll, arr_findAll_find)));
+                    System.out.println(Arrays.toString(solution.findAll(arrFindAll, arrFindAllFind)));
+                    break;
                 default:
                     break;
             }
